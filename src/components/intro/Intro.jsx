a81e84c134b14462db8c1a3300115e564345d410
sayasha <2two2two8eight@mail.ru>
2024-04-08 12:37:30 +0300

    main layout

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,15 @@
 import './intro.css'
 
 const Intro = () => {
-  return (
-	<div>Intro</div>
-  )
+	return (
+		<div className="wrapper">
+			<div className="text">
+				<h1>Найдите свою идеальную квартиру <br /> с помощью умного квиза!</h1>
+				<h3>Ответьте на несколько вопросов и мы подберем лучшие варианты жилья, <br /> отвечающие всем вашим требованиям.</h3>
+			</div>
+			<button>Найти</button>
+		</div>
+	)
 }
 
 export default Intro
