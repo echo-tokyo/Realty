472bb99df8dfab550c9a415fea863fb54035e493
Danil-114195722 <ejyou.user@gmail.com>
2024-04-09 14:26:05 +0300

    Added Makefile for run and test app

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from settings.constants import app
 from services import views
 
 
-app.add_url_rule("/", methods=["POST"], view_func=views.main)
+app.add_url_rule("/api", methods=["POST"], view_func=views.main)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='5555')
