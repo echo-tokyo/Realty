9d33e61ae124759ef7fa08f66b9068b8738b1e21
Danil-114195722 <ejyou.user@gmail.com>
2024-04-09 15:50:27 +0300

    Added nginx configs and Docker configs to deploy app on server (using SSL from Let's Encrypt)

--- backend/services/views.py
+++ backend/services/views.py
@@ -1,2 +1,16 @@
+from flask import request
+
+from services.mail import send_mail
+
+
 def main():
-    return "Hello"
+    body_json = request.get_json()
+
+    mail_text = 'ЗАТЫЧКА'
+
+    mail_subject = 'Опрос про недвижимость на сайте'
+    send_mail(subject=mail_subject, text=mail_text)
+
+    print(body_json)
+
+    return f"\n{body_json}\n"
