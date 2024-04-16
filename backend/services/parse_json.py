7916e2e135472dd1ec910edf2de9b103f39a206c
Danil-114195722 <ejyou.user@gmail.com>
2024-04-16 09:11:53 +0300

    Added received json handler

---
+++ backend/services/parse_json.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+from settings.constants import logger
+
+
+def json_to_mail_text(json_data: dict) -> str:
+    needed_json_keys = ["apartmentOrHouse", "budget", "districts", "newBuildingOrResale", "payment", "phone", "renovatedOrNot"]
+
+    if sorted(list(json_data)) != needed_json_keys:
+        logger.critical(f'Handle json error: "Required JSON keys are not equal to received JSON keys!"')
+        return "ERROR"
+
+    text = f"""Новая анкета о недвижимости.
+
+Квартира/дом: ----- {json_data["apartmentOrHouse"].capitalize()} \033[0m
+Бюджет: ----- {str(json_data["budget"])}
+Районы: ----- {", ".join(json_data["districts"])}
+Новое здание / вторичка: ----- {json_data["newBuildingOrResale"].capitalize()}
+Способы расчёта: ----- {", ".join(json_data["payment"])}
+Ремонт: ----- {json_data["renovatedOrNot"].capitalize()}
+
+Номер телефона: {"{0}({1}{2}{3}){4}{5}{6}-{7}{8}-{9}{10}".format(*list(str(json_data["phone"])))}
+
+
+Дата и время прохождения опроса: {datetime.strftime(datetime.now(), "%d.%m.%Y, %H:%M:%S")}. 
+"""
+
+    return text
--- backend/services/views.py
+++ backend/services/views.py
@@ -1,16 +1,21 @@
-from flask import request
+from flask import request, jsonify
 
 from services.mail import send_mail
+from services.parse_json import json_to_mail_text
 
 
 def main():
     body_json = request.get_json()
 
-    mail_text = 'ЗАТЫЧКА'
-
     mail_subject = 'Опрос про недвижимость на сайте'
-    send_mail(subject=mail_subject, text=mail_text)
 
-    print(body_json)
+    mail_text = json_to_mail_text(json_data=body_json)
+    if mail_text == "ERROR":
+        return jsonify({"status": "error"}), 400
 
-    return f"\n{body_json}\n"
+    send_ok = send_mail(subject=mail_subject, text=mail_text)
+    if not send_ok:
+        return jsonify({"status": "error"}), 500
+
+    print(mail_text)
+    return jsonify({"status": "ok"}), 200
