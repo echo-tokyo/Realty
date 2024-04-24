e6dd1d2631ed2f3028094a2506610773666f69f5
sayasha <2two2two8eight@mail.ru>
2024-04-24 22:45:24 +0300

    updated

--- frontend/src/components/end/End.jsx
+++ frontend/src/components/end/End.jsx
@@ -1,15 +1,23 @@
 import './end.css'
 import {useNavigate} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
+import { setFifth, setFirst, setFourth, setSecond, setSixth, setThird } from '../../store/quiz/quiz.slice'
 
 const End = () => {
 	const nav = useNavigate()
+	const dispatch = useDispatch()
 
 	return (
 		<div className="end">
 			<h1>Готово 🎉</h1>
 			<div className="btn">
 				<button onClick={() => {
-					location.reload()
+					dispatch(setFirst(false))
+					dispatch(setSecond(false))
+					dispatch(setThird(false))
+					dispatch(setFourth(false))
+					dispatch(setFifth(false))
+					dispatch(setSixth(false))
 					nav('/')
 				}}>Вернуться назад</button>
 			</div>
