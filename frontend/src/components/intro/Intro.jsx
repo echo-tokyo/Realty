19f8a904f1f2593113fe4b54f06cd17192c62dca
sayasha <2two2two8eight@mail.ru>
2024-04-30 14:38:39 +0300

    bg anim

--- frontend/src/components/intro/Intro.jsx
+++ frontend/src/components/intro/Intro.jsx
@@ -3,6 +3,16 @@ import {useNavigate} from 'react-router-dom'
 
 const Intro = () => {
 	const nav = useNavigate()
+	const slides = ['../../../public/bg1.png', '../../../public/bg2.png', '../../../public/bg3.png']
+	let url = 0
+	setInterval(() => {
+		url += 1
+		if(url == slides.length) {
+				url = 0
+		} 
+		
+		document.querySelector('.wrapper').style.backgroundImage = 'url(' + slides[url] + ')'
+	}, 3000);
 	return (
 		<div className="wrapper">
 			<div className="text">
